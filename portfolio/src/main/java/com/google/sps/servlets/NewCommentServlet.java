f59df497e55aadb73d654f4b802e96c54349fa55
Ionis-Kutrolli <ionis@google.com>
2020-06-09 10:49:29 -0400

    Add feature where users can only add comments when logged in and refactor code

--- portfolio/src/main/java/com/google/sps/servlets/NewCommentServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/NewCommentServlet.java
@@ -8,26 +8,36 @@ import javax.servlet.http.HttpServletResponse;
 import com.google.appengine.api.datastore.DatastoreService;
 import com.google.appengine.api.datastore.DatastoreServiceFactory;
 import com.google.appengine.api.datastore.Entity;
+import com.google.appengine.api.users.UserService;
+import com.google.appengine.api.users.UserServiceFactory;
 
 @WebServlet("/new-comment")
 public class NewCommentServlet extends HttpServlet {
 
   @Override
   public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
-    //Get input from form 
-    String comment = request.getParameter("comment");
-    String user = request.getParameter("user");
+    UserService userService = UserServiceFactory.getUserService();
+    if (userService.isUserLoggedIn()) {
+      //Get input from form 
+      String comment = request.getParameter("comment");
+      String user = request.getParameter("user");
 
-    long timestamp = System.currentTimeMillis();
+      long timestamp = System.currentTimeMillis();
 
-    Entity commentEntity = new Entity("Comment");
-    commentEntity.setProperty("comment", comment);
-    commentEntity.setProperty("timestamp", timestamp);
-    commentEntity.setProperty("user", user);
+      Entity commentEntity = new Entity("Comment");
+      commentEntity.setProperty("comment", comment);
+      commentEntity.setProperty("timestamp", timestamp);
+      commentEntity.setProperty("user", user);
 
-    DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
-    datastore.put(commentEntity);
+      DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+      datastore.put(commentEntity);
+    }
 
-    response.sendRedirect("/index.html");
+    response.sendRedirect(request.getHeader("referer"));
+  }
+
+  @Override
+  public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+
   }
 }
