7e91fea204bca0d014e847e1be2d2c0c4ef54b35
Ionis-Kutrolli <ionis@google.com>
2020-06-15 10:01:39 -0400

    Implement first solution for the FindMeetingQuery algorithm

--- walkthroughs/week-5-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
+++ walkthroughs/week-5-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
@@ -15,9 +15,49 @@
 package com.google.sps;
 
 import java.util.Collection;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public final class FindMeetingQuery {
   public Collection<TimeRange> query(Collection<Event> events, MeetingRequest request) {
-    throw new UnsupportedOperationException("TODO: Implement this method.");
+    long requestDuration = request.getDuration();
+    Collection<String> requestAttendees = request.getAttendees();
+    Collection<TimeRange> meetingOptions = new ArrayList<>();
+    if(requestDuration > TimeRange.WHOLE_DAY.duration()){
+      return meetingOptions;
+    }
+    meetingOptions.add(TimeRange.WHOLE_DAY);
+    events.forEach(event -> {
+      if (!Collections.disjoint(event.getAttendees(), requestAttendees)) {
+        removeTime(meetingOptions, event.getWhen(), (int)requestDuration);
+      }
+    });
+    return meetingOptions;
+  }
+
+  private void removeTime(Collection<TimeRange> currentOptions, TimeRange timeToRemove, int durationLimiter) {
+    Collection<TimeRange> toRemoveFromList = new ArrayList<>();
+    Collection<TimeRange> toAddToList = new ArrayList<>();
+    currentOptions.forEach( timeRange -> {
+      if(timeRange.overlaps(timeToRemove)) {
+        if(timeRange.contains(timeToRemove.start())) {
+          TimeRange newTime = TimeRange.fromStartEnd(timeRange.start(), timeToRemove.start(), false);
+          if(newTime.duration() >= durationLimiter) {
+            toAddToList.add(newTime);
+          }
+        }
+        if(timeRange.contains(timeToRemove.end())) {
+          TimeRange newTime = TimeRange.fromStartEnd(timeToRemove.end(), timeRange.end(), false);
+          if(newTime.duration() >= durationLimiter) {
+            toAddToList.add(newTime);
+          }
+        }
+        toRemoveFromList.add(timeRange);
+      }
+    });
+    currentOptions.removeAll(toRemoveFromList);
+    currentOptions.addAll(toAddToList);
   }
 }
