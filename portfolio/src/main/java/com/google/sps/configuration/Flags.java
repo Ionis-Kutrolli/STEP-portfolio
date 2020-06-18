1a7bf645eae5977f1a1ba13a8c114874200f9d5e
Ionis-Kutrolli <ionis@google.com>
2020-06-18 11:40:56 -0400

    Add support for js testing with selenium and mocha

---
+++ portfolio/src/main/java/com/google/sps/configuration/Flags.java
@@ -0,0 +1,9 @@
+package com.google.sps.configuration;
+
+/**
+ * Class that hold the flags for the configuration of the project
+ */
+public class Flags {
+  public static final boolean isReal = true;
+
+}
